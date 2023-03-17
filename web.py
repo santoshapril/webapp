dd758f5007b151fbd575688b8b9ad5c8a240998c
santosh <santoshsldk1997@gmail.com>
2023-03-17 20:07:13 +0530

    web app

--- web.py
+++ web.py
@@ -24,5 +24,8 @@ for index, todo in enumerate(todos):
 st.text_input(label="", placeholder='Add new todo...',
               on_change=add_todo, key='new_todo')
 
+print(hello)
+
 st.session_state
+
 
