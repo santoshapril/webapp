f29f75f9b72966dd4c208fdc27d0bc31b893f33f
santosh <santoshsldk1997@gmail.com>
2023-03-19 00:01:06 +0530

    web app

--- web.py
+++ web.py
@@ -20,7 +20,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-
 st.text_input(label="", placeholder='Add new todo...',
               on_change=add_todo, key='new_todo')
 
